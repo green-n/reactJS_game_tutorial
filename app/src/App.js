75b875c64684764b0b00dede1293903cdc95d56b
green-n <65806207+green-n@users.noreply.github.com>
2022-06-05 21:45:11 +0000

    it actialy works

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          it was a little fluke
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          it works like this
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
